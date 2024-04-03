ee92d246a7280c09b2410fb496b83a630a9eccf1
WyvernH <93463398+WyvernH@users.noreply.github.com>
2024-04-03 13:43:47 -0700

    fixed add bug

--- src/main.c
+++ src/main.c
@@ -6,8 +6,8 @@ int main(int argv, char **argc) {
 
   init_math();
 
-  string_t *num1 = init_string(U"123.012342");
-  string_t *num2 = init_string(U"892.298761");
+  string_t *num1 = init_string(U"16,2");
+  string_t *num2 = init_string(U"9,99");
 
   print(num1);
   printf("\n");
@@ -17,71 +17,13 @@ int main(int argv, char **argc) {
   string_t *sm = sum(num1, num2, NULL, NULL, NULL, NULL);
 
   print(sm);
-  printf("\n^ sum\n");
-
-  double dsm = string_to_double(sm);
-  printf("float: %f\n", dsm);
-
-  ip(sm);
-  int ism = string_to_int(sm);
-  printf("ip: ");
-  print(sm);
-  printf("\nint: %d\n", ism);
-
-  string_t *intstr = int_to_string(1);
-  print(intstr);
-  printf("\n");
-
-  string_t *doublestr = double_to_string(6, 3, false);
-  print(doublestr);
-  printf("\n");
-
-  string_free(intstr);
-  string_free(doublestr);
-
-  string_reverse(num1);
-  printf("num1 reversed: ");
-  print(num1);
   printf("\n");
 
   string_free(num1);
   string_free(num2);
   string_free(sm);
 
-  string_t *two = int_to_string(2);
-  string_t *three = int_to_string(3);
-  print(two);
-  printf("\n");
-  print(three);
-  printf("\n* =\n");
-
-  string_t *prod = product(two, three);
-  print(prod);
-  printf("\n");
-
-  string_free(two);
-  string_free(three);
-  string_free(prod);
-
   math_free();
-
-  FILE *FP = fopen("test.txt", "rb");
-  string_t *f = file_read(FP);
-  if (!f) return 1;
-  fclose(FP);
-  print(f);
-  string_free(f);
-
-  string_t *bigstr = init_string(U"100002000001000020000010000000000000000002222222200000000000000000000");
-  printf("l=%lu,b=%lu\n", bigstr->length, bigstr->bufsize);
-
-  print(bigstr);
-  printf("\n");
-
-  string_t *bigcopy = string_copy(bigstr);
-
-  string_free(bigcopy);
-  string_free(bigstr);
 
   return 0;
 }
